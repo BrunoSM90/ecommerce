67aeb7fe8aba85e5db8199ae5f5acf4eaa8e5eed
Bruno <smbrunosm@gmail.com>
2018-01-23 22:03:19 -0200

    Feature - Ajustes

--- src/app/series/series-module.ts
+++ src/app/series/series-module.ts
@@ -1,27 +1,33 @@
-import { SeriesFilterPipe } from './../pipes/series-filter.pipe';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared-module';
 
+import { SeriesFilterPipe } from './../pipes/series-filter.pipe';
 import { SeriesRoutingModule } from './series-routing-module';
 import { SeriesService } from './../services/series.service';
 
 import { SeriesDetailComponent } from './series-detail/series-detail.component';
 import { SeriesComponent } from './series.component';
+import { SerieformComponent } from './serieform/serieform.component';
+
 
 @NgModule ({
     imports: [
         SeriesRoutingModule,
         CommonModule,
         FormsModule,
-        RouterModule
+        RouterModule,
+        ReactiveFormsModule,
+        SharedModule
     ],
     exports: [],
     declarations: [
         SeriesComponent,
         SeriesDetailComponent,
         SeriesFilterPipe,
+        SerieformComponent,
     ],
     providers: [
         SeriesService
